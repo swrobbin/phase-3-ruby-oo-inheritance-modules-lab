5963f78f7cd46fab70180ba95828ac06ee04a550
Stephen <swrobbin@gmail.com>
2021-11-03 16:22:05 -0400

    commit tree ef53f4300f93db711d825780cc97d7bbc5f8b25a

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,6 @@
 module Findable 
     def find_by_name(name)
-        Song.detect{|a| a.name == name}
-    end
-
-    def find_by_name(name)
-        Artist.detect{|a| a.name == name}
+        self.all.detect{|a| a.name == name}
     end
 
 end    
--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,34 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods  
   extend Findable
+  include Paramable
+  # ::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
+  # def initialize
+  #   self.class.all << self
   # end
 
   def self.all
     @@songs
   end
 
-  # def self.reset_all
+  def artist=(artist)
+    @artist = artist
+  end
+
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+end
+
+# def self.reset_all
   #   self.all.clear
   # end
 
@@ -28,11 +36,6 @@ class Song
   #   self.all.count
   # end
 
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+ # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
